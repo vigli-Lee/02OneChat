d91810cc0fe95675403f6502363066ae133f17eb
vigli-Lee <wnsqjadl@gmail.com>
2019-11-24 17:21:28 +0900

    [func] 로그인 레이아웃 리니어로 생성하기

---
+++ app/src/main/java/com/mobile/vigli/onechat/LoginActivity.kt
@@ -0,0 +1,15 @@
+package com.mobile.vigli.onechat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.mobile.vigli.onechat.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+    }
+}
