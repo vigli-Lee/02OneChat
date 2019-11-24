e7d3acc19a226c165d28a4d84054a80d71d8d241
vigli-Lee <wnsqjadl@gmail.com>
2019-11-24 17:49:22 +0900

    [refec] LoginActivity 레이아웃 ConstraintLayout으로 변경

--- app/src/main/java/com/mobile/vigli/onechat/LoginActivity.kt
+++ app/src/main/java/com/mobile/vigli/onechat/LoginActivity.kt
@@ -4,12 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.mobile.vigli.onechat.databinding.ActivityLoginBinding
+import com.mobile.vigli.onechat.databinding.ActivityLoginConstraintBinding
 
 class LoginActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityLoginBinding
+    private lateinit var binding: ActivityLoginConstraintBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login_constraint)
     }
 }
